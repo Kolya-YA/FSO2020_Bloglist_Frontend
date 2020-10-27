3f3fd750e203ff4e670aa182410d31bab1f13799
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2020-10-27 22:48:44 +0100

    5.19: bloglist end to end testing, step3

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,9 +1,16 @@
-describe('My first Cypress test!',function() {
+describe('Start and login tests',function() {
   const testUser = {
     name: 'Pupkine W. User-1',
     login: 'pupkine-1@example.com',
     password: '123pupkine1'
   }
+
+  const testNewBlog = {
+    title: 'testTitle',
+    author: 'testAuthor',
+    url: 'testURL'
+  }
+
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3000/api/testing/reset')
     cy.request('POST', 'http://localhost:3000/api/users', testUser)
@@ -15,22 +22,35 @@ describe('My first Cypress test!',function() {
     cy.contains('Login to Bloglist application')
   })
 
-  describe('Login test' , function() {
+  describe('Login form test' , function() {
     it('Succeeds with correct credentials' , function() {
-      cy.get('input:first').clear().type(testUser.login)
-      cy.get('input:last').clear().type(testUser.password)
-      cy.get('button').contains('Login').click()
+      cy.loginUi(testUser)
       cy.contains(`${testUser.name} logged in.`)
       cy.get('button').contains('Logout').click()
     })
 
     it('Succeeds with wrong credentials' , function() {
-      cy.get('input:first').clear().type('Qwerty')
-      cy.get('input:last').clear().type('qwerty')
-      cy.get('button').contains('Login').click()
+      cy.loginUi({ login: 'Qwerty', password: 'qwerty123' })
       cy.get('.notification--error')
         .should('have.css', 'border-color', 'rgb(220, 20, 60)')
-        .contains('Invalid login or password.')
+        .and('have.css', 'border-style', 'solid')
+        .and('contain', 'Invalid login or password.')
+    })
+  })
+
+  describe.only('When loged in', function() {
+    beforeEach(function() {
+      cy.login(testUser)
+    })
+
+    it('Create new blog', function() {
+      cy.contains('button', 'Add new blog').click()
+      cy.contains('Create new blog')
+      cy.addNewBlogUi(testNewBlog)
+      cy.get('.blogList').children().should('have.length', 1)
+      cy.get('.blogList>.blogList__item')
+        .should('contain',testNewBlog.title)
+        .and('not.contain',testNewBlog.url)
     })
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,28 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('loginUi', user => {
+  cy.get('input:first').clear().type(user.login)
+  cy.get('input:last').clear().type(user.password)
+  cy.get('button').contains('Login').click()
+})
+
+Cypress.Commands.add('login', ({ login, password }) => {
+  cy.request({
+    url: 'http://localhost:3003/api/login',
+    method: 'POST',
+    body:  { login, password }
+  })
+    .then(({ body }) => {
+      localStorage.setItem('loggedUser', JSON.stringify(body))
+      cy.visit('http://localhost:3000')
+    })
+})
+
+Cypress.Commands.add('addNewBlogUi', newBlog => {
+  cy.get('input[name=title]').clear().type(newBlog.title)
+  cy.get('input[name=author]').clear().type(newBlog.author)
+  cy.get('input[name=url]').clear().type(newBlog.url)
+  cy.contains('button', 'Create new blog').click()
+})
